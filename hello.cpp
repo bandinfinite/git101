726a14810356970f176af3db8dbd47d62db99856
bandinfinite <kailash.162k7@gmail.com>
2026-07-21 19:28:07 +0530

    Created a new file demonstrating vectors

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<String> cars = {"Volvo", "Benz", "Maruti"};
+	for(String Car : cars}
+	{
+		cout << cars << endl;
+	}
+	return 0;
+}
